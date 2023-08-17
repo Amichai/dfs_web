7ae1c1e0747fec0df5780edf935fbad147adcf95
Amichai Levy <amichailevy@Amichais-MacBook-Pro.local>
2023-08-17 19:46:59 -0400

    wip

--- frontend/src/views/SlatesView.vue
+++ frontend/src/views/SlatesView.vue
@@ -1,26 +1,70 @@
 <script setup>
-  import axios from 'axios';
-  import { writeData, queryData, searchData } from '../apiHelper';
-  import { ref, onMounted, computed, nextTick, watch } from 'vue'
+import axios from 'axios';
+import { writeData, queryData, searchData } from '../apiHelper';
+import { ref, onMounted, computed, nextTick, watch } from 'vue'
 
-  const fetchData = async () => {
-    searchData('test1', 'test', 'test3' ).then((res) => {
-      console.log(res);
-    })
-    
-    // searchData('test1', 'time', '2023-08-17T02' ).then((res) => {
-    //   console.log(res);
-    // })
+const fetchData = async () => {
+  searchData('test1', 'test', 'test3').then((res) => {
+    console.log(res);
+  })
+
+  // searchData('test1', 'time', '2023-08-17T02' ).then((res) => {
+  //   console.log(res);
+  // })
+}
+
+onMounted(async () => {
+  await fetchData();
+})
+
+  /// handle uploads of slate files
+  /// diff the data and only write new data to the db
+  /// show today's slate files with date picker, sport picker, site picker, etc
+
+  const fileUploaded = (evt) => {
+    let files = evt.target.files; // FileList object
+      let f = files[0];
+      let reader = new FileReader();
+
+      reader.onload = (() => {
+        return function(e) {
+          const content = e.target.result
+          const lines = content.split('\n')
+          console.log(lines)
+        };
+      })();
+
+      reader.readAsText(f);
   }
 
-  onMounted(async () => {
-    await fetchData();
-  })
+  const clearFile = () => {
+    document.getElementById('formFile').value = ''
+  }
 </script>
 
 <template>
-  <div>TESTING</div>
+  <main>
+    <h2>Slates</h2>
+    <!-- date picker -->
+    <div class="upload-button">
+      <!-- :disabled="!Object.keys(byPlayerId).length" -->
+      <input class="form-control" 
+            @change="fileUploaded"
+          type="file" id="formFile">
+            <button class="btn btn-outline-danger" type="button" id="button-addon2" @click="clearFile">clear</button>
+    </div>
+  </main>
 </template>
 
-<style>
+<style scoped>
+.upload-button {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 1rem;
+}
+#formFile {
+  width: 100%;
+}
 </style>
